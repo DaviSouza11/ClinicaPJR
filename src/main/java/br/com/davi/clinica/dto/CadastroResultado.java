a18035dd6622fce5c39b37725a92816d05d7253c
Davi Souza <davisouzabelo133@gmail.com>
2026-04-25 13:38:08 -0300

    Organização e Limpeza de código 2.0

--- src/main/java/br/com/davi/clinica/dto/CadastroResultado.java
+++ src/main/java/br/com/davi/clinica/dto/CadastroResultado.java
@@ -1,4 +1,27 @@
 package br.com.davi.clinica.dto;
 
 public class CadastroResultado {
+    private int idUsuario;
+    private int codigoGerado;
+
+    public CadastroResultado(int idUsuario, int codigoGerado){
+        this.idUsuario = idUsuario;
+        this.codigoGerado = codigoGerado;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getCodigoGerado() {
+        return codigoGerado;
+    }
+
+    public void setCodigoGerado(int codigoGerado) {
+        this.codigoGerado = codigoGerado;
+    }
 }
--- src/main/java/br/com/davi/clinica/service/UsuarioService.java
+++ src/main/java/br/com/davi/clinica/service/UsuarioService.java
@@ -3,17 +3,30 @@ package br.com.davi.clinica.service;
 import br.com.davi.clinica.dao.UsuarioDAO;
 import br.com.davi.clinica.model.Usuario;
 import br.com.davi.clinica.security.SenhaBCrypt;
+import br.com.davi.clinica.dto.CadastroResultado;
+
+import java.util.Random;
 
 public class UsuarioService {
 
     private UsuarioDAO usuarioDAO = new UsuarioDAO();
+    private EmailService mailService = new EmailService();
 
-    public int cadastrar(Usuario usuario){
-
+    public CadastroResultado cadastrarComVerificacaoEmail(Usuario usuario){
         String hash = SenhaBCrypt.gerarHash(usuario.getSenha());
-
         usuario.setSenha(hash);
 
-        return usuarioDAO.cadastrar(usuario);
+        int idGerado = usuarioDAO.cadastrar(usuario);
+        int codigoSeguranca = 100000 + new Random().nextInt(999999);
+
+        try {
+            mailService.enviarCodigo(usuario.getEmail(), codigoSeguranca);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao enviar cógido",e);
+        }
+        return new CadastroResultado(idGerado, codigoSeguranca);
+    }
+    public void ativarConta(int idUsuario) {
+        usuarioDAO.ativarConta(idUsuario);
     }
 }
